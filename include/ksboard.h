a3dcb3c6f66dd13bdf2f1470e2e7e073a41b1420
unknown <arttostog>
2025-09-21 13:14:31 +0500

    feat: Добавлена настройка выходов портов (Чтение и запись пока не реализованны полноценно)

--- include/ksboard.h
+++ include/ksboard.h
@@ -1,6 +1,28 @@
 #pragma once
 
+#include <ksb_selected_board.h>
+
 void board_start();
 void board_loop();
 
 void delay(unsigned int milliseconds);
+
+typedef struct {
+    unsigned int is_output;
+    unsigned int function;
+    unsigned int is_digital;
+    unsigned int enable_pull_up;
+    unsigned int enable_pull_down;
+    unsigned int shm_mode;
+    unsigned int pd_mode;
+    unsigned int power_mode;
+    unsigned int use_filter;
+} pin_config_t;
+
+void pin_config(volatile mdr_port_t *port, unsigned int port_out,  const pin_config_t *config);
+
+unsigned int pin_digital_read(volatile mdr_port_t *port, unsigned int port_out);
+void pin_digital_write(volatile mdr_port_t *port, unsigned int port_out, unsigned int data);
+
+unsigned int pin_analog_read(volatile mdr_port_t *port, unsigned int port_out);
+void pin_analog_write(volatile mdr_port_t *port, unsigned int port_out, unsigned int data);
--- src/system/ksb_base.c
+++ src/system/ksb_base.c
@@ -1,4 +1,4 @@
-#include <ksb_selected_board.h>
+#include <ksboard.h>
 
 void delay(unsigned int milliseconds) {
     SYS_TICK->load = LSI_FREQUENCY - 1;
@@ -10,4 +10,38 @@ void delay(unsigned int milliseconds) {
             --milliseconds;
     
     SYS_TICK->ctrl = 0;
+}
+
+void pin_config(volatile mdr_port_t *port, unsigned int port_out, const pin_config_t *config) {
+    port_out &= 15;
+    
+    port->oe = (port->oe & ~(1 << port_out)) | ((config->is_output & 1) << port_out);
+
+    port->func = (port->func & ~(3 << (port_out * 2))) | ((config->function & 3) << (port_out * 2));
+
+    port->analog = (port->analog & ~(1 << port_out)) | ((config->is_digital & 1) << port_out);
+
+    port->pull = (port->pull & ~((1 << port_out) + (1 << port_out + 16))) | (((config->enable_pull_down & 1) << port_out) + ((config->enable_pull_up & 1) << (port_out + 16)));
+
+    port->pd = (port->pd & ~((1 << port_out) + (1 << (port_out + 16)))) | (((config->pd_mode & 1) << port_out) + ((config->shm_mode & 1) << (port_out + 16)));
+
+    port->pwr = (port->pwr * ~(3 << (port_out * 2))) | ((config->power_mode & 3) << (port_out * 2));
+
+    port->gfen = (port->gfen & ~(1 << port_out)) | ((config->use_filter & 1) << port_out);
+}
+
+unsigned int pin_digital_read(volatile mdr_port_t *port, unsigned int port_out) {
+    return (port->rxtx & (1 << port_out)) >> port_out;
+}
+
+void pin_digital_write(volatile mdr_port_t *port, unsigned int port_out, unsigned int data) {
+    port->rxtx = (port->rxtx & ~(1 << port_out)) | ((data & 1) << port_out);
+}
+
+unsigned int pin_analog_read(volatile mdr_port_t *port, unsigned int port_out) {
+    return 0;
+}
+
+void pin_analog_write(volatile mdr_port_t *port, unsigned int port_out, unsigned int data) {
+
 }
