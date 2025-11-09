f2bbb6bdcc486c8b019d2bed7d1b30e30eb0629e
arttostog <arttostog@gmail.com>
2025-11-09 22:27:03 +0500

    feat: Наброски утилиты для загрузки прошивки на плату

---
+++ load_tool.py
@@ -0,0 +1,42 @@
+from sys import argv, exit
+from serial import Serial, PARITY_NONE, STOPBITS_ONE, EIGHTBITS
+
+class LoadTool:
+    def start(port: str, file: str) -> None:
+        bytes_to_write: bytes = LoadTool.read_file()
+
+        try:
+            device: Serial = Serial(
+                port=port,
+                baudrate=9600,
+                parity=PARITY_NONE,
+                stopbits=STOPBITS_ONE,
+                bytesize=EIGHTBITS,
+                timeout=1,
+            )
+
+            device.write(b'\x0D\x0A\x0E')
+            device.write(b'\x4C')
+            device.write(LoadTool.format_value(0x08000000))
+            device.write(LoadTool.format_value(len(bytes_to_write)))
+            device.write(bytes_to_write)
+            
+            device.write(b'\x52')
+            device.write(LoadTool.format_value(0x08000000))
+
+        except Exception as exception:
+            print(f"Возникло исключение: {exception}")
+
+    
+    def read_file(file: str) -> bytes:
+        with open(file, "rb") as file:
+            return file.read()
+
+    def format_value(value: int) -> bytes:
+        return value.to_bytes(4, byteorder='little')
+
+if __name__ == "__main__":
+    if len(argv) != 3:
+        print("Too few or many args!")
+        exit(1)
+    LoadTool.start(argv[1], argv[2])
