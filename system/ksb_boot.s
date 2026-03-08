b88cde0401e3f2dbdc7d37b4e6cc5f017165057d
arttostog <arttostog@gmail.com>
2026-03-09 00:04:34 +0500

    refactor: Функции в ksb_boot.s были оптимизированны и  внедрены в Reset_Handler

--- system/ksb_boot.s
+++ system/ksb_boot.s
@@ -4,54 +4,54 @@
 
 .section .isr_vector
 vectors:
-    .word _stack_top
-    .word Reset_Handler // RESET
-    .word base_handler  // NMI
-    .word base_handler  // Hard Fault
-    .word base_handler  // Memory Management Fault
-    .word base_handler  // Bus Fault
-    .word base_handler  // Usage Fault
-    .word 0
-    .word 0
-    .word 0
-    .word 0
-    .word base_handler  // SVCall
-    .word 0
-    .word 0
-    .word base_handler  // PendSV
-    .word base_handler  // SysTick
-    .word base_handler  // CAN1
-    .word base_handler  // CAN2
-    .word base_handler  // USB
-    .word 0
-    .word 0
-    .word base_handler  // DMA
-    .word base_handler  // UART1
-    .word base_handler  // UART2
-    .word base_handler  // SSP1
-    .word 0
-    .word base_handler  // I2C
-    .word base_handler  // POWER
-    .word base_handler  // WWDG
-    .word 0
-    .word base_handler  // Timer 1
-    .word base_handler  // Timer 2
-    .word base_handler  // Timer 3
-    .word base_handler  // ADC
-    .word 0
-    .word base_handler  // COMP
-    .word base_handler  // SSP2
-    .word 0
-    .word 0
-    .word 0
-    .word 0
-    .word 0
-    .word 0
-    .word base_handler  // BACKUP
-    .word base_handler  // EXT_INT1
-    .word base_handler  // EXT_INT2
-    .word base_handler  // EXT_INT3
-    .word base_handler  // EXT_INT4
+.word _stack_top,
+    Reset_Handler, // RESET
+    base_handler,  // NMI
+    base_handler,  // Hard Fault
+    base_handler,  // Memory Management Fault
+    base_handler,  // Bus Fault
+    base_handler,  // Usage Fault
+    0,
+    0,
+    0,
+    0,
+    base_handler,  // SVCall
+    0,
+    0,
+    base_handler,  // PendSV
+    base_handler,  // SysTick
+    base_handler,  // CAN1
+    base_handler,  // CAN2
+    base_handler,  // USB
+    0,
+    0,
+    base_handler,  // DMA
+    base_handler,  // UART1
+    base_handler,  // UART2
+    base_handler,  // SSP1
+    0,
+    base_handler,  // I2C
+    base_handler,  // POWER
+    base_handler,  // WWDG
+    0,
+    base_handler,  // Timer 1
+    base_handler,  // Timer 2
+    base_handler,  // Timer 3
+    base_handler,  // ADC
+    0,
+    base_handler,  // COMP
+    base_handler,  // SSP2
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    base_handler,  // BACKUP
+    base_handler,  // EXT_INT1
+    base_handler,  // EXT_INT2
+    base_handler,  // EXT_INT3
+    base_handler  // EXT_INT4
 
 .section .boot, "ax"
 
@@ -62,57 +62,48 @@ vectors:
 .global Reset_Handler
 
 Reset_Handler:
-    bl bss_clear
-    bl data_copy
+; Очистка bss
+    ldr r0, =_bss_start
+    ldr r1, =_bss_end
+    mov r2, #0
+.bss_clear_loop:
+    cmp r0, r1
+    beq .bss_clear_end
 
+    str r2, [r0], #4
+    blt .bss_clear_loop
+.bss_clear_end:
+; Копирование data
+    ldr r0, =_data_start_flash
+    ldr r1, =_data_start
+
+    cmp r0, r1
+    beq .data_copy_end
+
+    ldr r2, =_data_size
+    cmp r2, #0
+    beq .data_copy_end
+.data_copy_loop:
+    ldr r3, [r0], #4
+    str r3, [r1], #4
+    subs r2, r2, #4
+    bne .data_copy_loop
+.data_copy_end:
+    mov r0, #0
+    mov r1, r0
+    mov r2, r0
+    mov r3, r0
     bl board_load
+
+    mov r0, #0
+    mov r1, r0
     bl board_start
 
-loop:
+    mov r0, #0
+    mov r1, r0
+.loop:
     bl board_loop
-    b loop
-
-.thumb_func
-bss_clear:
-    push {r1-r3, lr}
-    ldr r1, =_bss_start
-    ldr r2, =_bss_end
-
-    cmp r1, r2
-    beq bss_clear_end
-    mov r3, #0
-
-bss_clear_loop:
-    str r3, [r1], #4
-    cmp r1, r2
-    blt bss_clear_loop
-
-bss_clear_end:
-    pop {r1-r3, lr}
-    bx lr
-
-.thumb_func
-data_copy:
-    push {r1-r4, lr}
-    ldr r1, =_data_start_flash
-    ldr r2, =_data_start
-
-    cmp r1, r2
-    beq data_copy_end
-
-    ldr r3, =_data_size
-    cmp r3, #0
-    beq data_copy_end
-
-data_copy_loop:
-    ldr r4, [r1], #4
-    str r4, [r2], #4
-    subs r3, r3, #4
-    bne data_copy_loop
-
-data_copy_end:
-    pop {r1-r4, lr}
-    bx lr
+    .b loop
 
 base_handler:
     b .
